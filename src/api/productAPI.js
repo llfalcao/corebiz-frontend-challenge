fe1de0a38ceab71323234b703b75ee5d412553a3
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2022-06-22 19:40:20 -0300

    Create Products component

---
+++ src/api/productAPI.js
@@ -0,0 +1,1 @@
+export const baseUrl = 'https://corebiz-test.herokuapp.com/api/v1/products';
---
+++ src/components/Products.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { baseUrl } from '../api/productAPI';
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(baseUrl);
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      {products.map((product) => (
+        <li key={product.productId}>
+          <img src={product.imageUrl} alt={product.productName} />
+          <p>{product.productName}</p>
+          {/* todo: star rating component */}
+          {product.listPrice ? <p>de R$ {product.listPrice}</p> : null}
+          <p>por R$ {product.price}</p>
+          {product.installments.length > 0 ? (
+            <p>
+              ou em {product.installments[0].quantity}x de R${' '}
+              {product.installments[0].value}
+            </p>
+          ) : null}
+        </li>
+      ))}
+    </div>
+  );
+}
